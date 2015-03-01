60276886008e59da8a65b020d6fddc4a7a3b8a76
Daniele Megna <megna.dany@gmail.com>
2015-03-01 18:33:39 +0100

    Adding tests and features for 11-20 numbers

--- src/Worker.php
+++ src/Worker.php
@@ -12,8 +12,14 @@ class Worker {
     if($n >= 4 && $n <= 8)
       return $this->workWithAPivot($n, 5, 'V');
 
-    if($n >= 9)
+    if($n >= 9 && $n <= 13)
       return $this->workWithAPivot($n, 10, 'X');
+
+    if($n >= 14 && $n <= 18)
+      return 'X' . $this->workWithAPivot($n, 15, 'V');
+
+    if($n >= 19 && $n <= 20)
+      return 'X' . $this->workWithAPivot($n, 20, 'X');
   }
 
   function workWithAPivot($n, $pivotValue, $pivotRappresentation)
--- tests/WorkerTest.php
+++ tests/WorkerTest.php
@@ -39,7 +39,7 @@ class WorkerTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('IV', $this->w->work(4));
   }
 
-  function testFirstTenNumbers()
+  function testFirstTenNumbersShouldBeConvertedProperly()
   {
     $this->assertEquals('V', $this->w->work(5));
     $this->assertEquals('VI', $this->w->work(6));
@@ -47,6 +47,20 @@ class WorkerTest extends PHPUnit_Framework_TestCase
     $this->assertEquals('VIII', $this->w->work(8));
     $this->assertEquals('IX', $this->w->work(9));
     $this->assertEquals('X', $this->w->work(10));
+  }
+
+  function testFromElevenToTwentyShouldWorkProperly()
+  {
+    $this->assertEquals('XI', $this->w->work(11));
+    $this->assertEquals('XII', $this->w->work(12));
+    $this->assertEquals('XIII', $this->w->work(13));
+    $this->assertEquals('XIV', $this->w->work(14));
+    $this->assertEquals('XV', $this->w->work(15));
+    $this->assertEquals('XVI', $this->w->work(16));
+    $this->assertEquals('XVII', $this->w->work(17));
+    $this->assertEquals('XVIII', $this->w->work(18));
+    $this->assertEquals('XIX', $this->w->work(19));
+    $this->assertEquals('XX', $this->w->work(20));
   }
 }
 
