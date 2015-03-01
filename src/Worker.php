4036cbc5fd768a4c43c983faa4a5966ab3d726f7
Daniele Megna <megna.dany@gmail.com>
2015-03-01 18:38:48 +0100

    Findings rule with pivot annoted with comments for refactoring

--- src/Worker.php
+++ src/Worker.php
@@ -7,15 +7,19 @@ class Worker {
     if($n >= 1 && $n <= 3)
       return $this->workWithAPivot($n, 0, '');
     
+    // $n+1 mod 5 = 1 ---> dispari
     if($n >= 4 && $n <= 8)
       return $this->workWithAPivot($n, 5, 'V');
 
+    // $n+1 mod 5 = 2 ---> pari
     if($n >= 9 && $n <= 13)
       return $this->workWithAPivot($n, 10, 'X');
 
+    // $n+1 mod 5 = 3 ---> dispari
     if($n >= 14 && $n <= 18)
       return 'X' . $this->workWithAPivot($n, 15, 'V');
 
+    // $n+1 mod 5 = 4 --> pari
     if($n >= 19 && $n <= 20)
       return 'X' . $this->workWithAPivot($n, 20, 'X');
   }
