90c6a3ca3e8d4b22cfbda2c5ae4ef4d1d4c38b69
Daniele Megna <megna.dany@gmail.com>
2015-03-01 18:14:44 +0100

    First step of refactoring

--- src/Worker.php
+++ src/Worker.php
@@ -6,20 +6,25 @@ class Worker {
   {
     $s = '';
 
-    if($n <= 3)
+    if($n >= 1 && $n <= 3) {
+      $s = '';
       while($n-- > 0) { $s .= 'I'; }
+    }
+
     else if($n == 4)
       $s = 'IV';
+      
     else if($n == 5)
       $s = 'V';
-    else if($n == 6)
-      $s = 'VI';
-    else if($n == 7)
-      $s = 'VII';
-    else if($n == 8)
-      $s = 'VIII';
+
+    else if($n >= 6 && $n <= 8) {
+      $s = 'V';
+      while($n-- > 5) { $s .= 'I'; }
+    }
+
     else if($n == 9)
       $s = 'IX';
+
     else if($n == 10)
       $s = 'X';
     
