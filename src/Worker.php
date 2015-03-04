b7b1d7c4218330f0667e71f21d6ba34dc363882c
Daniele Megna <megna.dany@gmail.com>
2015-03-04 23:52:55 +0100

    Really... i don't know how it's working....

--- src/Worker.php
+++ src/Worker.php
@@ -7,32 +7,16 @@ class Worker {
     if($n >= 1 && $n <= 3)
       return $this->workWithAPivot($n, 0, '');
 
-    $nPlusOneDividedBy5 = floor(($n+1) / 5);
+    $nFiveFloor = floor(($n+1) / 5);
+    $isFloorOdd = ($nFiveFloor % 2) == 1;
+    $pivotRappresentation = $isFloorOdd ? 'V' : 'X';
+    $pivotValue = $nFiveFloor * 5;
 
-    // $n+1 div 5 = 1 ---> dispari
-    if($nPlusOneDividedBy5 == 1)
-      return $this->workWithAPivot($n, 5, 'V');
+    $prefix = '';
+    if($pivotValue > 10)
+      $prefix = $this->work($isFloorOdd ? $pivotValue-5 : $pivotValue-10);
 
-    // $n+1 div 5 = 2 ---> pari
-    if($nPlusOneDividedBy5 == 2)
-      return $this->workWithAPivot($n, 10, 'X');
-
-    // $n+1 div 5 = 3 ---> dispari
-    if($nPlusOneDividedBy5 == 3)
-      return 'X' . $this->workWithAPivot($n, 15, 'V');
-
-    // $n+1 div 5 = 4 --> pari
-    if($nPlusOneDividedBy5 == 4)
-      return 'X' . $this->workWithAPivot($n, 20, 'X');
-
-    // $n+1 div 5 = 5 --> dispari
-    if($nPlusOneDividedBy5 == 5)
-      return 'XX' . $this->workWithAPivot($n, 25, 'V');
-
-    // $n+1 div 5 = 6 --> pari
-    if($nPlusOneDividedBy5 == 6)
-      return 'XX' . $this->workWithAPivot($n, 30, 'X');
-
+    return $prefix . $this->workWithAPivot($n, $pivotValue, $pivotRappresentation);
   }
 
   function workWithAPivot($n, $pivotValue, $pivotRappresentation)
