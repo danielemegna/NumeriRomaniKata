1b1ade490c5ae2387bc43a21da3426186b4f00c2
Daniele Megna <megna.dany@gmail.com>
2015-03-05 00:22:11 +0100

    Return to extended solution.. we have to refactor now

--- src/Worker.php
+++ src/Worker.php
@@ -7,20 +7,47 @@ class Worker {
     if($n >= 1 && $n <= 3)
       return $this->workWithAPivot($n, 0, '');
 
-    $nFiveFloor = floor(($n+1) / 5);
-    $isFloorOdd = ($nFiveFloor % 2) == 1;
-    $pivotRappresentation = $isFloorOdd ? 'V' : 'X';
-    $pivotValue = $nFiveFloor * 5;
+    $nPlusOneDividedBy5 = floor(($n+1) / 5);
 
-    $prefix = '';
-    if($pivotValue > 10)
-      $prefix = $this->work($isFloorOdd ? $pivotValue-5 : $pivotValue-10);
+    // $n+1 div 5 = 1 ---> odd
+    if($nPlusOneDividedBy5 == 1)
+      return $this->workWithAPivot($n, 5, 'V');
 
-    $result = $prefix . $this->workWithAPivot($n, $pivotValue, $pivotRappresentation);
-    if($result == 'XXXXX')
-      $result = 'L';
+    // $n+1 div 5 = 2 ---> even
+    if($nPlusOneDividedBy5 == 2)
+      return $this->workWithAPivot($n, 10, 'X');
 
-    return $result;
+    // $n+1 div 5 = 3 ---> odd
+    if($nPlusOneDividedBy5 == 3)
+      return 'X' . $this->workWithAPivot($n, 15, 'V');
+
+    // $n+1 div 5 = 4 --> even
+    if($nPlusOneDividedBy5 == 4)
+      return 'X' . $this->workWithAPivot($n, 20, 'X');
+
+    // $n+1 div 5 = 5 --> odd
+    if($nPlusOneDividedBy5 == 5)
+      return 'XX' . $this->workWithAPivot($n, 25, 'V');
+
+    // $n+1 div 5 = 6 --> even
+    if($nPlusOneDividedBy5 == 6)
+      return 'XX' . $this->workWithAPivot($n, 30, 'X');
+
+    // $n+1 div 5 = 7 --> odd
+    if($nPlusOneDividedBy5 == 7)
+      return 'XXX' . $this->workWithAPivot($n, 35, 'V');
+
+    // $n+1 div 5 = 8 --> even
+    if($nPlusOneDividedBy5 == 8)
+      return 'XXX' . $this->workWithAPivot($n, 40, 'X');
+
+    // $n+1 div 5 = 9 --> odd
+    if($nPlusOneDividedBy5 == 9)
+      return 'XXXX' . $this->workWithAPivot($n, 45, 'V');
+
+    // $n+1 div 5 = 10 --> even
+    if($nPlusOneDividedBy5 == 10)
+      return 'L';
   }
 
   function workWithAPivot($n, $pivotValue, $pivotRappresentation)
